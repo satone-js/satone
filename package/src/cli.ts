117820f2d4635a34215da2547d8c7a0d13b520e7
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-08-18 14:21:36 +0200

    chore: add wip commands for CLI

--- package/src/cli.ts
+++ package/src/cli.ts
@@ -1,3 +1,22 @@
 #!/usr/bin/env bun
-import { createDevServer } from "./server/dev";
-await createDevServer();
+//! This file is the entrypoint for the `satone` CLI, defined in `package.json`.
+
+const [, , command] = Bun.argv;
+
+if (!command) {
+  console.log("Welcome to Satone CLI.");
+  console.log("- satone build: builds your app for production");
+  console.log("- satone dev: runs a devlopment server with hot reload");
+  process.exit(0);
+}
+
+switch (command) {
+  case "dev": {
+    const { createDevServer } = await import("./server/dev");
+    await createDevServer();
+    break;
+  }
+  case "build": {
+    console.log("TODO");
+  }
+}
