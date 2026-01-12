bdb11828917ae8d9f7b7eaeb03502eea600f975d
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2026-01-12 23:15:48 +0100

    fix(router): glob for `.ts` files

--- package/src/utils/constants.ts
+++ package/src/utils/constants.ts
@@ -2,7 +2,7 @@ import { join } from "node:path";
 import { Glob } from "bun";
 
 export const PROJECT_PATH = process.cwd();
-export const GLOB = new Glob("**/*.ts[x]");
+export const GLOB = new Glob("**/*.{ts,tsx}");
 export const ROUTES_PATH = join(PROJECT_PATH, "src/routes");
 export const CACHE_FOLDER = join(PROJECT_PATH, "node_modules", ".satone");
 export const SERVER_FOLDER = join(CACHE_FOLDER, "server");
