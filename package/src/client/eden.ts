6cb7037fe4a6db42e0336ce0a08c875300eee9da
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2026-01-13 13:42:51 +0100

    fix: typo on eden origin

--- package/src/client/eden.ts
+++ package/src/client/eden.ts
@@ -1,8 +1,7 @@
 import { treaty } from "@elysiajs/eden";
 
-const origin: string
-  // @ts-expect-error : if this runs on the server, let's forget...
-  = typeof window !== "undefined" ? window.location.origin : undefined;
+// @ts-expect-error : if this runs on the server, let's forget...
+const origin: string = typeof window !== "undefined" ? window.location.origin : undefined;
 
 // @ts-expect-error : only defined in user's project, thanks to tsconfig.json
 //                    and generated `.satone` files
